405a8dbc1a8a0413de59d3359346d7e2eadac95f
Niraj Wagh <iamnirajwagh@gmail.com>
2021-09-23 11:51:11 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
 var express= require("express");
-
 const { config } = require("winston");
-
 const winston = require("winston");
-
 const bodyParser = require("body-parser");
-
 var app=express();
-
 const logger = require('./winston/config')
 
-
-const https = require("https")
-
-const fs = require("fs")
-
-const path = require("path")
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
 
-app.use('/', (req, res, next) => {
 
+app.get("/", function(req, res){
+    res.render("login");
+    logger.log('info', req.rawHeaders)
+    
+}).listen(3000);
+
+app.get("/home", function(req, res){
+    res.render("home");
+});
+
+app.post("/loginsuccess", function(req, res){
+
+
+    logger.log('info', "Login Success: "+req.body.email)
+    logger.log('info', req.socket.remoteAddress)
+    res.render("home")
+
+})
+
+app.post("/loginfailed", function(req, res){
+    
+    logger.log('info', req.body.error);
     res.render("login")
 
 })
 
-const server = https.createServer({
-
-    key: fs.readFileSync(path.join(__dirname, 'certs', 'www.editissbank.com.key.pem')),
-
-    cert: fs.readFileSync(path.join(__dirname, 'certs', 'www.editissbank.com.cert.pem'))
-
-}, app)
-
-server.listen(443, () => console.log("running on 443"))
-
-
+console.log("Server Started at localhost:3000");
